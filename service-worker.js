7b49273c939fdb1fdcd01a5d50494812cf4acfdc
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-06-02 14:27:08 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,33 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/scripts.js'
+            ]);
+        })
+    );
+});
 
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['my-cache'];
+    event.waitUntil(
+        caches.keys().then((keyList) =>
+            Promise.all(keyList.map((key) => {
+                if (!cacheWhitelist.includes(key)) {
+                    return caches.delete(key);
+                }
+            }))
+        )
+    );
+});
