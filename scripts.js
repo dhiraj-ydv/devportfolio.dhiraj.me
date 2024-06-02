30012a7d22ed49c874c92b2dc0b8377f03319f04
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-06-02 14:28:09 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-// No JavaScript needed for this static version
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }).catch((error) => {
+            console.log('Service Worker registration failed:', error);
+        });
+}
+
