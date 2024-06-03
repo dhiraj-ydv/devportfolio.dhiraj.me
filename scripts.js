951d650e64f0b807ad1a70769902cb7724a1e4ed
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-06-03 17:22:18 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (let registration of registrations) {
+            registration.unregister();
+        }
+    }).then(() => {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
             console.log('Service Worker registered with scope:', registration.scope);
         }).catch((error) => {
             console.log('Service Worker registration failed:', error);
         });
+    });
 }
-
