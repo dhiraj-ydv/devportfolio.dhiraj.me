b82e26d3166b82067ee46c02703748dc88ea8afd
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-06-25 16:14:56 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,12 @@
-const CACHE_NAME = 'my-site-cache-v6';
+
+const CACHE_NAME = 'my-site-cache-v2';
 const urlsToCache = [
     '/',
-    '/styles/main.css',
-    '/scripts/scripts.js',
-    // Add other assets you want to cache
+    '/index.html',
+    '/styles.css',
+    '/scripts.js',
+    '/profile.jpg',
+    '/favicon.ico'
 ];
 
 // Install and cache the files
