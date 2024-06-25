df85f733a1269b139c5faa81b4df0ef8cf41d751
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-06-25 16:00:32 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,32 +1,10 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(registration => {
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
-            if (navigator.serviceWorker.controller) {
-              // New content is available; please refresh.
-              console.log('New or updated content is available.');
-              if (confirm('New version available. Do you want to update?')) {
-                window.location.reload();
-              }
-            } else {
-              // Content is cached for offline use.
-              console.log('Content is cached for offline use.');
-            }
-          }
-        };
-      };
-    })
-    .catch(error => {
-      console.error('Error during service worker registration:', error);
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, error => {
+            console.log('ServiceWorker registration failed: ', error);
+        });
     });
-
-  let refreshing;
-  navigator.serviceWorker.addEventListener('controllerchange', () => {
-    if (refreshing) return;
-    window.location.reload();
-    refreshing = true;
-  });
 }
+
