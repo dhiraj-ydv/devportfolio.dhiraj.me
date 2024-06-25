703e43b954dbcb85149d2700fcb4efa4726043f8
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-06-25 16:21:20 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,26 @@
-
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/service-worker.js').then(registration => {
-            registration.update();
-            registration.addEventListener('updatefound', () => {
-                const newWorker = registration.installing;
-                newWorker.addEventListener('statechange', () => {
-                    if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-                        window.location.reload();
+            registration.onupdatefound = () => {
+                const installingWorker = registration.installing;
+                installingWorker.onstatechange = () => {
+                    if (installingWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                        const notification = document.createElement('div');
+                        notification.style.position = 'fixed';
+                        notification.style.bottom = '0';
+                        notification.style.width = '100%';
+                        notification.style.backgroundColor = 'black';
+                        notification.style.color = 'white';
+                        notification.style.textAlign = 'center';
+                        notification.style.padding = '1em';
+                        notification.innerText = 'New version available. Refresh to update.';
+                        notification.onclick = () => {
+                            window.location.reload();
+                        };
+                        document.body.appendChild(notification);
                     }
-                });
-            });
+                };
+            };
         }).catch(error => {
             console.log('ServiceWorker registration failed: ', error);
         });
