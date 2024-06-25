61e8268ee8429608b5f0286b3dc27a02e053edb5
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-06-25 16:07:59 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/service-worker.js').then(registration => {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, error => {
+            registration.update();
+            registration.addEventListener('updatefound', () => {
+                const newWorker = registration.installing;
+                newWorker.addEventListener('statechange', () => {
+                    if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                        window.location.reload();
+                    }
+                });
+            });
+        }).catch(error => {
             console.log('ServiceWorker registration failed: ', error);
         });
     });
 }
-
